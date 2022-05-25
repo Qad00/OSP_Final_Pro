023a13300ded1de47113df6c25b410c242703450
Qad00 <majidovkadridin@gmail.com>
2022-05-25 16:20:37 +0900

    init

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template, request
-from features.crawling import craw_Comments
-from features.research import research
-from dataset import db
-
+from features.crawling import craw_comments
+from features.comment_research import research_data
+from features.word_cloud import make_word_cloud
+from features.url_type import check_url_type
+from features.url_type import get_video_id
 
 app = Flask(__name__)
 
+
+# BASE = http://127.0.0.1:5000
 
 # go to homepage and get youtube video link
 # action post
@@ -20,17 +23,30 @@ def index():
 def result():
     given_url = request.form(['url'])
     # get video from youtube with given url and redirect to result page
-    video = ""
-    # go to crawling file and take word_cloud
-    word_cloud = craw_Comments(given_url)
-    # take research result
-    research_res = research(given_url)
 
+    # check is url is youtube type url
+    if not check_url_type(given_url):
+        # if not return 404 error to home page with explanation correct form of youtube video link
+        return "home_html", 404
 
-    # db manage
-     
-    #return data should replace: local vars to db data...
-    return render_template("result_page.html", video=video, word_cloud=word_cloud, research_res=research_res)
+    # FIRST check db is exist such research already in db
+        # if no
+            # get video id for using in html
+    video_id = get_video_id(given_url)
+
+            # crawling video comments with given url
+    crawled_data = craw_comments(given_url)
+
+            # make word cloud from crawled data
+    word_cloud = make_word_cloud(crawled_data)
+
+            # get research result that was done
+    research_res = research_data(crawled_data)
+
+            # add new research with parameters to db
+
+    # return parameters and asked research, to result_page from db
+    return render_template("result_page.html", video_id=video_id, word_cloud=word_cloud, research_res=research_res)
 
 
 if __name__ == '__main__':
