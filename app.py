a290ea9a26cdcb48596ef13222661881de213e97
khj1030 <khj011030@gmail.com>
2022-06-19 23:07:29 +0900

    html update

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-<<<<<<< HEAD
 import argparse
 import subprocess
 from flask import Flask, render_template, request
@@ -28,7 +27,7 @@ j = 1
 def index():
     global i
     crawData = Crawling()
-    elastic = Elastic_class(es_host=f'http://172.20.0.3:9200/')
+    elastic = Elastic_class()
     # craw title, img, link, likes, views from top 10 videos in realtime
     crawData.setHVideo()
 
@@ -43,36 +42,18 @@ def index():
     print("Home page checked successfully")
     crawData.closeDriver()
     return render_template('home_page.html', videos_data=videos_data)
-=======
-from flask import Flask, render_template, request
-#from features.crawling import craw_comments
-#from features.comment_research import research_data
-#from features.word_cloud import make_word_cloud
-#from features.url_type import check_url_type
-#from features.url_type import get_video_id
-from features.db.elasticsearch import *
-
-app = Flask(__name__)
-
-
-# BASE = http://127.0.0.1:5000
-
-# go to homepage and get youtube video link
-# action post
->>>>>>> 709c3ebe47c62799a2114a7d1bda098f0880e697
 
 # @app.route('/')
 # def index():
     
 #     return render_template('result_page.html')
 
-<<<<<<< HEAD
-@app.route('/searched_word_result_page', methods=['GET', 'POST'])
+@app.route('/searched_word_page', methods=['GET', 'POST'])
 def required_videos():
     global j
     # get word that was searched form searching field
     crawData = Crawling()
-    elastic = Elastic_class(es_host='http://172.20.0.3:9200/')
+    elastic = Elastic_class()
     #option = "by default"
     keyword = "By default"
     if request.method == 'POST':
@@ -96,35 +77,11 @@ def required_videos():
     print("Searched word result page --> Success")
     crawData.closeDriver()
     print("Driver closed")
-    return render_template("searched_word_result_page.html", keyword=keyword, videos_data=videos_data[keyword])
-=======
-
-# @app.route('/result_page', methods=['POST'])
-# def result():
-#     given_url = request.form(['url'])
-#     # get video from youtube with given url and redirect to result page
-
-#     # check is url is youtube type url
-#     if not check_url_type(given_url):
-#         # if not return 404 error to home page with explanation correct form of youtube video link
-#         return "home_html", 404
-
-#     # FIRST check db is exist such research already in db
-#         # if no
-#             # get video id for using in html
-#     video_id = get_video_id(given_url)
-
-#             # crawling video comments with given url
-#     crawled_data = craw_comments(given_url)
-
-#             # make word cloud from crawled data
-#     word_cloud = make_word_cloud(crawled_data)
->>>>>>> 709c3ebe47c62799a2114a7d1bda098f0880e697
+    return render_template("searched_word_page.html", keyword=keyword, videos_data=videos_data[keyword])
 
 #             # get research result that was done
 #     research_res = research_data(crawled_data)
 
-<<<<<<< HEAD
 k = 1
 l = 1
 # run after one of video was clicked
@@ -134,7 +91,7 @@ def result():
     clicked_video_link = 'By default'
     craw_data = Crawling()
     pre = Preprocessing()
-    elastic = Elastic_class(es_host='http://172.20.0.3:9200/')
+    elastic = Elastic_class()
     # data could be accessed in html from db
     # or return by variables from result function
     if request.method == 'POST':
@@ -186,30 +143,3 @@ if __name__ == '__main__':
     # ipaddr = "127.0.0.1"
     print("Starting the service with ip_addr=" + ipaddr)
     app.run(debug=False, host=ipaddr, port=int(listen_port))
-=======
-#             # add new research with parameters to db
-
-#     # return parameters and asked research, to result_page from db
-#     return render_template("result_page.html", video_id=video_id, word_cloud=word_cloud, research_res=research_res)
-
-
-if __name__ == '__main__':
-    # test={ url:{"title":title,
-    #             "image" : image,
-    #             "hits":hits,
-    #             "good" : good,
-    #             "result number": num,
-    #             "positive/negetive" : pn,
-    #             "positive percent" : p_percent,
-    #             "positive word": list(p_word),
-    #             "negetive word": list(n_word),
-    #             "word cloud" : file_path
-    #         }
-    #     }
-    # es_host ="http://localhost:9200"
-    # es = Elasticsearch(es_host)
-    test=url_result("url",'title_name','image_name',1000,50,100,1,80,[1,2,3],[4,5,6],"example")
-    print(insert('result',1,test))
-    search('result')
-    # app.run()
->>>>>>> 709c3ebe47c62799a2114a7d1bda098f0880e697
